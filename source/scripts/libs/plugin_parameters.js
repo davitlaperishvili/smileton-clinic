8b4f552441a15f28dbbcd0a771ff502e37d6abe1
dlaperishvili <davitlaperishvili@gmail.com>
2022-07-11 22:10:33 +0400

    swiper updated

--- source/scripts/libs/plugin_parameters.js
+++ source/scripts/libs/plugin_parameters.js
@@ -1,48 +1,45 @@
-export default function plugindParameters(){
+//Swiper Slider
+import Swiper from "swiper/bundle";
+
+export default function plugindParameters() {
 	// Selector styling
-	$("select.selectori").each(function(){
-		var placeholder = $(this).data('placeholder') || 'Default placeholder';
+	$("select.selectori").each(function () {
+		var placeholder = $(this).data("placeholder") || "Default placeholder";
 		$(this).select2({
 			allowClear: true,
 			minimumResultsForSearch: -1,
-			placeholder: placeholder
-		}); 
+			placeholder: placeholder,
+		});
 	});
 	// Slider
-	$(".main_slider").slick({
-		autoplay: true,
-		dots: false,
-		arrows: true,
-		infinite: true,
-		speed: 1000,
-		slidesToShow: 1,
-		fade: true,
-		adaptiveHeight: true,
-		prevArrow: '<em class="ri-arrow-left-s-line"></em>',
-		nextArrow: '<em class="ri-arrow-right-s-line"></em>'
+	let mySwiper = new Swiper(".mySwiper", {
+		slidesPerView: 1,
+		pagination: {
+			el: ".swiper-pagination",
+		},
 	});
 	// Gallery
-	$(".magnific").each(function(){
+	$(".magnific").each(function () {
 		$(this).magnificPopup({
 			delegate: "a",
 			type: "image",
-			gallery:{
-				enabled: true
-			},
-			zoom:{
+			gallery: {
 				enabled: true,
-				duration: 300
-			}
+			},
+			zoom: {
+				enabled: true,
+				duration: 300,
+			},
 		});
 	});
-	if($(".magnific_video").length){
+	if ($(".magnific_video").length) {
 		$(".magnific_video").magnificPopup({
 			disableOn: 700,
-			type: 'iframe',
-			mainClass: 'mfp-fade',
+			type: "iframe",
+			mainClass: "mfp-fade",
 			removalDelay: 160,
 			preloader: false,
-			fixedContentPos: false
+			fixedContentPos: false,
 		});
 	}
 }
